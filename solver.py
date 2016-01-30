b1447d2148563d7b445601a3d0a94c7174a2e85f
Trevor Ross <trevor.is.ross@gmail.com>
2016-01-30 11:29:02 -0600

    added more comments and moved some code around

--- solver.py
+++ solver.py
@@ -44,9 +44,10 @@ class StateTree():
     def __init__(self, initial_puzzle, number_of_colors):
         # a globla ID index for creating unique node IDs
         self.ID = 0
+        # create a root node
         self.root = Node(self.ID, state=initial_puzzle)
-        # self.puzzle = initial_puzzle
         self.num_colors = number_of_colors
+        # find start and end locations of puzzle colors
         self.color_start = FindColorStart(self.root.state, self.num_colors)
         self.root.path_heads = self.color_start
         self.color_end = FindColorEnd(self.root.state, self.num_colors)
@@ -54,40 +55,45 @@ class StateTree():
         # dictionary of states indexed by their ID
         self.state_dict = {self.root.ID:self.root}
 
+        # TIMING VARIABLES
+        self.total_time_on_final = 0.0
+        self.total_time_on_action = 0.0
+        self.total_time_on_creation = 0.0
+        self.total_time_on_result = 0.0
+        self.BFTS_start_time = time.time()
 
+    # PURPOSE: finds optimal solution to puzzle by using breadth first search
+    # Each state is first enqued, in a FIFO queue. Later, the element will be
+    # dequeued. Once it is dequeued, all it's valid moves will be found and
+    # a child state will be created and enqueued for each one.
+    # OUTPUT: either False or list of states leading to solution
     def BreadthFirstTreeSearch(self):
+        self.BFTS_start_time = time.time()
         queue = [self.root]
-        interupt_state = 20000
-        last_interrupt = time.time()
-        total_time_on_final = 0.0
-        total_time_on_action = 0.0
-        total_time_on_creation = 0.0
-        BFTS_start_time = time.time()
 
         # loop until final state is found or queue is emptied
         while True:
-            # first check if queue is empty
+            # If queue is empty, no solution exits
             if len(queue) == 0:
-                # if so then no solution exits
+                self.EndSequence(False)
                 return False
+
             # dequeue the front element
             to_examine = queue.pop(0)
-            # Visualize(to_examine.state)
-            # examine element
-            colors_connected = self.VerifyFinal(to_examine.state)
-            # go through each color, finding actions for each
+            # get a list of colors in puzzle and shuffle it
             color_numbers = range(self.num_colors)
             random.shuffle(color_numbers)
-            # Visualize(to_examine.state)
+            # iterate through colors in puzzle, checking for actions on each
             for color_num in color_numbers:
+                # decide if any colors are connected
+                colors_connected = self.VerifyFinal(to_examine.state)
                 # ignore colors that have already found their goal state
                 if color_num in colors_connected: continue
+
                 # get the coordinates of the furthest point of the color's path
                 coord = to_examine.path_heads[color_num]
                 # retrive all valid actions from this color's path head
-                time_before_action_check = time.time()
-                valid_actions, valid_coords = Actions(to_examine.state, coord, self.color_end)
-                total_time_on_action += (time.time() - time_before_action_check)
+                valid_actions, valid_coords = self.Actions(to_examine.state, coord)
                 # print '%d: VALID ACTIONS: %r' % (color_num, DirPrint(valid_actions))
                 # print 'valid extentions of %d:' % color_num, valid_coords
                 # create a new child state for each valid action
@@ -97,7 +103,7 @@ class StateTree():
                     action_coord = valid_coords[i]
                     self.ID += 1
                     # retulting child state from parent acted on by action
-                    c_state = Result(to_examine.state, coord, action)
+                    c_state = self.Result(to_examine.state, coord, action)
                     # create new node
                     child = Node(ID=self.ID, parent_node=to_examine.ID, state=c_state, action=action)
                     # updated the child's path heads
@@ -108,30 +114,93 @@ class StateTree():
                     # add child to the dict
                     self.state_dict[child.ID] = child
                     # check if child is Goal State
-                    time_before_final_check = time.time()
                     colors_connected = self.VerifyFinal(child.state)
                     if colors_connected == True:
                         # a goal state has been found
                         # return the final state and it's ancestors
-                        print '-- TIME --'
-                        print 'TOTAL:', (time.time() - BFTS_start_time)
-                        print 'FINAL CHECK: ', total_time_on_final
-                        print 'ACTION CHECK:', total_time_on_action
-                        print 'CREATION:    ', total_time_on_creation
-                        print '-- STATES --'
-                        print 'CREATED: ', self.ID
-                        print 'EXPANDED:', to_examine.ID
+                        self.EndSequence(True)
                         return (self.TraceBack(child))
-                    total_time_on_final += (time.time() - time_before_final_check)
                     # push child onto queue
                     queue.append(child)
-                    total_time_on_creation += (time.time() - time_before_creation)
+                    self.total_time_on_creation += (time.time() - time_before_creation)
 
+    # PURPOSE: given a state, a coordinate, and an end_position, the function
+    # will return a list of all valid moves.
+    # FORMAT: the 4 possible moves are: [[-1,0], [0,1], [1,0], [0,-1]]
+    # VALID MOVE DISQUALIFICATION:
+    # 1) moves out of puzzle's bounds
+    # 2) moves onto a pre-existing line
+    # 3) path moves adjacent to itself, aka, the path 'touches' itself
+    # OUTPUT: returns a list of valid actions as well as the coordinates they result in
+    def Actions(self, p_state, coord):
+        time_before_action_check = time.time()
+        upper_bound = len(p_state)
+        lower_bound = 0
+        color =  int(p_state[coord[0]][coord[1]])
+        end_coord = self.color_end[color]
+        valid_actions = []
+        valid_coords = []
+
+        # actions in order: down, right, up, left
+        action_options = [[-1,0], [0,1], [1,0], [0,-1]]
+        random.shuffle(action_options)
+        for action in action_options:
+            new_row = coord[0]+action[0]
+            new_col = coord[1]+action[1]
+            # check if move is out-of-bounds
+            if new_col < lower_bound or new_col == upper_bound:
+                continue
+            if new_row < lower_bound or new_row == upper_bound:
+                continue
+            # check if space is already occupied
+            if p_state[new_row][new_col] != 'e':
+                continue
+            # check if move results in path becoming adjacent to itself
+            adj_itself = 0
+            for adj in action_options:
+                adj_row = new_row+adj[0]
+                adj_col = new_col+adj[1]
+                # check if adjacent square is out-of-bounds
+                if adj_col < lower_bound or adj_col == upper_bound:
+                    continue
+                if adj_row < lower_bound or adj_row == upper_bound:
+                    continue
+                if p_state[adj_row][adj_col] == str(color):
+                    if [adj_row, adj_col] != end_coord:
+                        adj_itself += 1
+            if adj_itself > 1:
+                continue
+            # if move is in-bounds, space is not occupied, and path isn't
+            # adjacent to itself, it is a valid move
+            new_coord = [new_row, new_col]
+            valid_actions.append(action)
+            valid_coords.append(new_coord)
+
+        self.total_time_on_action += (time.time() - time_before_action_check)
+        return (valid_actions, valid_coords)
+
+    # PURPSOSE: return the result of taking action on the coordinate of the
+    # given state
+    # OUTPUT: returns state in the form: [[... row 1 ...], [... row 2 ...], ...]
+    def Result(self, p_state, coord, action):
+        time_before_result = time.time()
+        new_state = copy.deepcopy(p_state)
+        # retrieve the 'color' of the path to be extended
+        color_path_to_extend = p_state[coord[0]][coord[1]]
+        # find the location to place the extention
+        new_row = coord[0]+action[0]
+        new_col = coord[1]+action[1]
+        # 'color' the new loaction, extending the line
+        new_state[new_row][new_col] = color_path_to_extend
+
+        self.total_time_on_result += (time.time() - time_before_result)
+        return new_state
 
     # PURPOSE: verify that the passed state is a final state
     # IF FINAL: return True
     # IF NOT FINAL: return a list of those colors who are final
-    def VerifyFinal(self, pzzl_state): # REVIEW: ask if i should name the function FINAL
+    def VerifyFinal(self, pzzl_state):
+        time_before_final_check = time.time()
         colors_connected = []
         upper_bound = len(pzzl_state)
         lower_bound = 0
@@ -151,21 +220,39 @@ class StateTree():
                     # color has been connected
                     colors_connected.append(color)
                     break
+
+        self.total_time_on_final += (time.time() - time_before_final_check)
         if len(colors_connected) == self.num_colors:
             return True
         else:
             return colors_connected
 
+    # PURPOSE: given the final node. find path from the final node to the root
+    # OUTPUT: list of all the nodes from root to final -> [root, ... , final]
+    def TraceBack(self, end_node):
+        node_path = []
+        node = end_node
+        # keep adding node to node_path until root node is found
+        while node.action != None:
+            # insert in front of list since traversal is bottom-up
+            node_path.insert(0, node)
+            # move to partent node
+            node = self.state_dict[node.p_ID]
+        # add the root
+        node_path.insert(0, node)
+        return node_path
 
-    def TraceBack(self, end_state):
-        state_path = []
-        state = end_state
-        while state.action != None:
-            state_path.insert(0, state)
-            state = self.state_dict[state.p_ID]
-        state_path.insert(0, state)
-
-        return state_path
+    # PURPOSE: print out time and states statistics so program can be analized
+    def EndSequence (self, sol_found):
+        print '-- TIME --'
+        print 'TOTAL:       ', (time.time() - self.BFTS_start_time)
+        print 'FINAL CHECK: ', self.total_time_on_final
+        print 'ACTION CHECK:', self.total_time_on_action
+        print 'RESULT:      ', self.total_time_on_result
+        print 'CREATION:    ', self.total_time_on_creation
+        print '-- STATES --'
+        print 'CREATED: ', self.ID
+        print 'EXPANDED:', (self.state_dict[self.ID]).p_ID
 
 
     def StateLookup(self):
@@ -260,71 +347,6 @@ def FindColorEnd(puzzle, num_colors):
 
     return coordinates
 
-# PURPOSE: given a state, a coordinate, and an end_position, the function
-# will return a list of all valid moves.
-# FORMAT: the 4 possible moves are: [[-1,0], [0,1], [1,0], [0,-1]]
-# VALID MOVE DISQUALIFICATION:
-# 1) moves out of puzzle's bounds
-# 2) moves onto a pre-existing line
-# 3) path moves adjacent to itself, aka, the path 'touches' itself
-# OUTPUT: returns a list of valid actions as well as the coordinates they result in
-def Actions(p_state, coord, end_positions):
-    upper_bound = len(p_state)
-    lower_bound = 0
-    color =  int(p_state[coord[0]][coord[1]])
-    end_coord = end_positions[color]
-    valid_actions = []
-    valid_coords = []
-
-    # actions in order: down, right, up, left
-    action_options = [[-1,0], [0,1], [1,0], [0,-1]]
-    random.shuffle(action_options)
-    for action in action_options:
-        new_row = coord[0]+action[0]
-        new_col = coord[1]+action[1]
-        # check if move is out-of-bounds
-        if new_col < lower_bound or new_col == upper_bound:
-            continue
-        if new_row < lower_bound or new_row == upper_bound:
-            continue
-        # check if space is already occupied
-        if p_state[new_row][new_col] != 'e':
-            continue
-        # check if move results in path becoming adjacent to itself
-        adj_itself = 0
-        for adj in action_options:
-            adj_row = new_row+adj[0]
-            adj_col = new_col+adj[1]
-            # check if adjacent square is out-of-bounds
-            if adj_col < lower_bound or adj_col == upper_bound:
-                continue
-            if adj_row < lower_bound or adj_row == upper_bound:
-                continue
-            if p_state[adj_row][adj_col] == str(color):
-                if [adj_row, adj_col] != end_coord:
-                    adj_itself += 1
-        if adj_itself > 1:
-            continue
-        # if move is in-bounds, space is not occupied, and path isn't
-        # adjacent to itself, it is a valid move
-        new_coord = [new_row, new_col]
-        valid_actions.append(action)
-        valid_coords.append(new_coord)
-
-    return (valid_actions, valid_coords)
-
-
-def Result(p_state, coord, action):
-    new_state = copy.deepcopy(p_state)
-    # retrieve the 'color' of th path to be extended
-    color_path_to_extend = p_state[coord[0]][coord[1]]
-    # find the location to place the extention
-    new_row = coord[0]+action[0]
-    new_col = coord[1]+action[1]
-    # 'color' the new loaction, extending the line
-    new_state[new_row][new_col] = color_path_to_extend
-    return new_state
-
 
 def DirPrint(directions):
     dir_array = []
@@ -410,3 +432,8 @@ else:
 
 ###################################
 ### TODO
+
+# finish the UGLY PRINT option
+# get the runtime down to under 10 seconds for puzzle 2
+# add more comments
+# make code more readable
